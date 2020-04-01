22ac15b5f58412eb591c908744a36c2a570a4189
gorkemaygun <gorkemaygun94@gmail.com>
2020-04-01 14:28:12 -0400

    updates

--- labExercise.js
+++ labExercise.js
@@ -1,5 +1,5 @@
 var slideshow={
-	photoList:['cat.jpg','dog.jpg','car.jpg','sea.jpg'],
+	photoList:['cat.jpg','dog.jpg','car.jpg','sea.jpg','tree.jpg','sun.jpg'],
 	currentPhotoIndex:0,
 
 	currentPhoto:function(){
@@ -10,32 +10,39 @@ var slideshow={
 			
 			if (this.currentPhotoIndex <this.photoList.length-1) {
 				this.currentPhotoIndex+=1;
+				this.play();
 				console.log('Next Photo is : ' + this.photoList[this.currentPhotoIndex]);
 				
-			}else{
+			}
+			else{
 				console.log('End of the slideshow');
-				}		
+				this.pause();
+				}			
 			},
 	prevPhoto:function(){
 			
 			if (this.currentPhotoIndex <this.photoList.length) {
 				this.currentPhotoIndex--;
 				console.log('Prev Photo is : ' +this.photoList[this.currentPhotoIndex]);	
-			}else{
+			}
+			else{
 				console.log('End of the slideshow');
 				}		
-			},		
-	}
-console.log(slideshow.photoList);	
+			},
+	playInterval : null,
+	play:function(){
+		
+		this.playInterval=setTimeout(function(){
+			this.nextPhoto();}.bind(this),2000);
+		
+	 } ,
+	pause:function(){
+		clearInterval(this.playInterval);
+	},
+}
+
 slideshow.currentPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.nextPhoto();
-slideshow.prevPhoto();
-slideshow.currentPhoto();
-slideshow.nextPhoto();
-slideshow.currentPhoto();
-slideshow.nextPhoto();
-slideshow.prevPhoto();
-slideshow.currentPhoto();
-slideshow.nextPhoto();
+
+slideshow.play();
+
+
